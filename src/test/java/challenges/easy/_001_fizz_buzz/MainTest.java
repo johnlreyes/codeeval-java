f914e6832cba268cd82ced5e9bdd66f4a1a082d7
John Reyes <johnlreyes@yahoo.com>
2017-10-01 18:48:42 +0800

    refactored

--- src/test/java/challenges/easy/_001_fizz_buzz/MainTest.java
+++ src/test/java/challenges/easy/_001_fizz_buzz/MainTest.java
@@ -3,6 +3,7 @@ package challenges.easy._001_fizz_buzz;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,19 +15,21 @@ import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
+	public final static String FOLDER_PATH = "src/test/java/challenges/easy/_001_fizz_buzz/";
+
 	@Test
 	void provided() throws Exception {
 		assertEquals(getExpected(), getActual());
 	}
 
 	private String getActual() throws IOException {
-		InputStream actualResource = this.getClass().getResourceAsStream("input");
+		InputStream actualResource = new FileInputStream(FOLDER_PATH + "input");
 		List<String> inputList = getFileContentAsList(actualResource);
 		return new Main().execute(inputList);
 	}
 
-	private String getExpected() {
-		InputStream expectedResource = this.getClass().getResourceAsStream("output");
+	private String getExpected() throws Exception {
+		InputStream expectedResource = new FileInputStream(FOLDER_PATH + "output");
 		return getFileContentAsList(expectedResource).stream()
 				.collect(Collectors.joining(System.getProperty("line.separator")));
 	}
